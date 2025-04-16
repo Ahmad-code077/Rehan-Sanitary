528aa5005a0fac7216db73e982d673fe137cd9ee
Ahmad Mamoon <ahmadeveloper077@gmail.com>
2025-04-17 01:39:04 +0500

    Chagne to nunito font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        nunito: ['p', 'sans-serif'], // Register the font
+        nunito: ['Nunito', 'sans-serif'], // Register the font
       },
 
       colors: {
